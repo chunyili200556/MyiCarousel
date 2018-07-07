cfbb9e46bf3e67c761ea91fec85f50d940d12075
chunyili <chunyili200556@gmail>
2018-07-07 13:06:58 +0800

    Initial Commit

---
+++ MyiCarousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyiCarousel
+//
+//  Created by 黎峻亦 on 2018/7/7.
+//  Copyright © 2018年 黎峻亦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
